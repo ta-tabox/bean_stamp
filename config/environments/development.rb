c7198ebdec77fb8a2ed7e50a2d6501b3bfb7027c
tatadkn <tatadaikn@gmail.com>
2021-10-20 22:02:53 +0900

    feat(devise): add of Signup link nav

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,13 +3,14 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # bulletの設定
   config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
+    Bullet.enable = true
+    Bullet.alert = true
     Bullet.bullet_logger = true
-    Bullet.console       = true
+    Bullet.console = true
+
     # Bullet.growl         = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -48,7 +49,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
+
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
