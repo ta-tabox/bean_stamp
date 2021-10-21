d2406fa3ca8666e51acc4cb3b8ab1f08549328bb
tatadkn <tatadaikn@gmail.com>
2021-10-21 22:56:23 +0900

    feat(devise): add users_controller and users_views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               passwords: 'users/passwords',
+               registrations: 'users/registrations',
+             }
   root 'static_pages#home'
   get 'home', to: 'static_pages#home', as: 'home'
   get 'help', to: 'static_pages#help', as: 'help'
