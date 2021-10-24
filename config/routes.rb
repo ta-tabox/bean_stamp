12209db7c6d13dea5100bdb09b6638ad269aea2a
tatadkn <tatadaikn@gmail.com>
2021-10-24 16:42:25 +0900

    fix(routes): change of users show_rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/home'
-  get 'users/show'
-  devise_for :users, path: 'users', module: 'users'
-
-  # devise_scope :user do
-  #   get 'users/home', to: 'users/users#home'
-  #   get 'users/show', to: 'users/users#show'
-  # end
-
   root 'static_pages#home'
   get 'home', to: 'static_pages#home', as: 'home'
   get 'help', to: 'static_pages#help', as: 'help'
   get 'about', to: 'static_pages#help', as: 'about'
+  devise_for :users, path: 'users', module: 'users'
+  resources :users, only: %i[show]
+  get 'users/home'
 end
