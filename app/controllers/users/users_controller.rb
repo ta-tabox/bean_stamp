340330ddaf4d4bfd80dd230380521a4cf8cfdf70
tatadkn <tatadaikn@gmail.com>
2021-10-24 16:34:08 +0900

    feat(users): add users controller and home show actions

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def home; end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               passwords: 'users/passwords',
-               registrations: 'users/registrations',
-             }
+  get 'users/home'
+  get 'users/show'
+  devise_for :users, path: 'users', module: 'users'
+
+  # devise_scope :user do
+  #   get 'users/home', to: 'users/users#home'
+  #   get 'users/show', to: 'users/users#show'
+  # end
+
   root 'static_pages#home'
   get 'home', to: 'static_pages#home', as: 'home'
   get 'help', to: 'static_pages#help', as: 'help'
