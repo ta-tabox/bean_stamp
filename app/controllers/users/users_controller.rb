912fc393e69558eb2ac84f0866c7926d4826b64c
tatadkn <tatadaikn@gmail.com>
2021-10-28 20:43:34 +0900

    fix(users_controller): change current_user to @user for user#show

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,5 +2,7 @@ class Users::UsersController < ApplicationController
   before_action :authenticate_user!
   def home; end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
