f6368b57b6eeb34da2882cbfaaaec57c1a5d36f3
tatadkn <tatadaikn@gmail.com>
2021-11-02 22:42:46 +0900

    feat(users_table): add roaster_id column to users_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_131521) do
+ActiveRecord::Schema.define(version: 2021_11_02_133745) do
 
   create_table "roasters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -34,8 +34,11 @@ ActiveRecord::Schema.define(version: 2021_11_02_131521) do
     t.string "area", default: ""
     t.text "describe"
     t.string "image"
+    t.bigint "roaster_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["roaster_id"], name: "index_users_on_roaster_id"
   end
 
+  add_foreign_key "users", "roasters"
 end
