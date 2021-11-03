128949935cb421ca09352f14209fd321f915547c
tatadkn <tatadaikn@gmail.com>
2021-11-03 22:16:25 +0900

    feat(roasters#create): add create action for roasters

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -3,13 +3,30 @@ class RoastersController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @roaster = current_user.build_roaster
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    @roaster = current_user.build_roaster(roaster_params)
+    if @roaster.save
+      flash[:success] = 'ロースター登録が完了しました'
+      current_user.save
+      redirect_to @roaster
+    else
+      render 'new'
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def roaster_params
+    params.require(:roaster).permit(:name, :phone_number, :address, :describe)
+  end
 end
