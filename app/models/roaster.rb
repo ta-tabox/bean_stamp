428a43e32cf5de166a19396deba5cf7214cdd0e6
tatadkn <tatadaikn@gmail.com>
2021-11-04 22:37:26 +0900

    feat(roaster model): add validation for roaster model

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,3 +1,22 @@
 class Roaster < ApplicationRecord
+  before_validation :before_validations
   has_many :users, dependent: :nullify
+  validates :name, presence: true
+  validates :phone_number,
+            presence: true,
+            numericality: {
+              only_integer: true,
+            },
+            length: {
+              in: 10..11,
+            }
+  validates :address, presence: true
+  validates :describe, length: { maximum: 300 }
+
+  private
+
+  def before_validations
+    # phone_numberの文字列から先頭と文末のスペースを除去
+    phone_number.strip!
+  end
 end
