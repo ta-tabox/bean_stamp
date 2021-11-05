c23e2c479b32cf127cc73d71ed55b14ed5cc5e9d
tatadkn <tatadaikn@gmail.com>
2021-11-06 08:31:22 +0900

    style(beautify): add beautify for .erb and add ruby plug_in for prettier

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,7 +59,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: %i[name area describe roaster_id image image_cache],
+      keys: %i[name area describe roaster_id image image_cache]
     )
   end
 
