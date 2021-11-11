19c5f6785f0457fd92963a252cbd491c5955b7c9
tatadkn <tatadaikn@gmail.com>
2021-11-11 20:15:10 +0900

    feat(beans_controller): add action(new, create) for beans resource

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,9 +1,54 @@
 class BeansController < ApplicationController
-  def index; end
+  before_action :set_roaster
 
-  def show; end
+  def index
+    @beans = @roaster.beans
+  end
 
-  def new; end
+  def show
+    @bean = @roaster.beans.find(params[:id])
+  end
+
+  def new
+    @bean = @roaster.beans.build
+  end
+
+  def create
+    @bean = @roaster.beans.build(bean_params)
+    if @bean.save
+      flash[:notice] = 'コーヒー豆を登録しました'
+      redirect_to @bean
+    else
+      render 'new'
+    end
+  end
 
   def edit; end
+
+  private
+
+  def bean_params
+    params
+      .require(:bean)
+      .permit(
+        :name,
+        :country,
+        :subregion,
+        :farm,
+        :variety,
+        :elevation,
+        :process,
+        :cropped_at,
+        :describe,
+        :acidity,
+        :flavor,
+        :body,
+        :bitterness,
+        :sweetness,
+      )
+  end
+
+  def set_roaster
+    @roaster = current_user.roaster
+  end
 end
--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,3 +1,15 @@
 class Bean < ApplicationRecord
   belongs_to :roaster
+  default_scope -> { order(created_at: :desc) }
+  validates :roaster_id, presence: true
+  validates :name, presence: true
+  validates :country, presence: true
+  validates :describe, length: { maximum: 300 }
+  with_options inclusion: { in: (1..5) } do
+    validates :acidity
+    validates :flavor
+    validates :body
+    validates :bitterness
+    validates :sweetness
+  end
 end
