64745072630d2f39da7cc3d7f3fd3c160ff9c112
tatadkn <tatadaikn@gmail.com>
2021-11-14 13:34:22 +0900

    refactor(roaster.rb): before_validationsにおける空白を削除するメソッドの削除

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,6 +1,5 @@
 class Roaster < ApplicationRecord
   include JpPrefecture
-  before_validation :before_validations
   has_many :users, dependent: :nullify
   validates :name, presence: true
   validates :phone_number,
@@ -16,11 +15,4 @@ class Roaster < ApplicationRecord
   validates :describe, length: { maximum: 300 }
   mount_uploader :image, ImageUploader
   jp_prefecture :prefecture_code
-
-  private
-
-  def before_validations
-    # phone_numberの文字列から先頭と文末のスペースを除去
-    phone_number.strip!
-  end
 end
