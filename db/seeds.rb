2e36c85c428e687e42b054c6970d20ef90fd55cf
tatadkn <tatadaikn@gmail.com>
2021-11-18 22:55:33 +0900

    feat(beans resource): コーヒー豆のサンプルデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,4 @@
-# guest roaster
-# Roaster.create!(
-#   name: 'ゲストロースター',
-#   phone_number: '0123456789',
-#   prefecture_code: '13',
-#   address: '**区**丁目**-**',
-#   describe: '閲覧用のロースターです',
-#   guest: true,
-# )
-
-# ゲストユーザーの作成
-User.create!(
-  name: 'ゲストユーザー',
-  email: 'guest@example.com',
-  prefecture_code: '13',
-  password: 'password',
-  password_confirmation: 'password',
-  describe: '閲覧用のロースターです',
-  # roaster_id: 1,
-  guest: true,
-)
-
-# ゲストロースターの作成
-user = User.find_by(guest: true)
-user.create_roaster!(
-  name: 'ゲストロースター',
-  phone_number: '0123456789',
-  prefecture_code: '13',
-  address: '**区**丁目**-**',
-  describe: '閲覧用のロースターです',
-  guest: true,
-)
-user.save!
+# 全ての環境で/seeds/base.rbを読み込む
+load(Rails.root.join('db/seeds/base.rb'))
+# Rails の環境に合わせて読み込むseedsファイルを切り替える
+load(Rails.root.join('db/seeds', "#{Rails.env.downcase}.rb"))
