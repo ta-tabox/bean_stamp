dcf12a1112a369919bfa4ebcc8718f8977ea8600
tatadkn <tatadaikn@gmail.com>
2021-11-25 23:42:21 +0900

    feat(bean_images): bean_imageの複数画像登録機能の実装

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,22 +1,33 @@
 class BeansController < ApplicationController
+  MAX_BEANIMAGE_COUNT = 4
   before_action :user_signed_in_required
   before_action :user_belonged_to_roaster_required
   before_action :set_bean, only: %i[show edit update destroy]
+  before_action :set_new_images, only: %i[create update]
 
   def index
     @pagy, @beans = pagy(current_roaster.beans)
   end
 
-  def show; end
+  def show
+    @bean_images = @bean.bean_images.all
+  end
 
   def new
     @bean = current_roaster.beans.build
+    @bean_image = @bean.bean_images.build
   end
 
   def create
     set_cropped_at
     @bean = current_roaster.beans.build(bean_params)
+
+    return if max_bean_images_count_render_for('new')
+
     if @bean.save
+      @new_images.each do |img|
+        @bean_image = @bean.bean_images.create(image: img, bean_id: @bean.id)
+      end
       flash[:notice] = 'コーヒー豆を登録しました'
       redirect_to @bean
     else
@@ -24,11 +35,17 @@ class BeansController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @bean_images = @bean.bean_images.all
+  end
 
   def update
     set_cropped_at
+
+    return if max_bean_images_count_render_for('edit')
+
     if @bean.update(bean_params)
+      update_bean_images if @new_images
       flash[:notice] = 'コーヒー豆情報を更新しました'
       redirect_to @bean
     else
@@ -62,8 +79,9 @@ class BeansController < ApplicationController
         :body,
         :bitterness,
         :sweetness,
-        { images: [] },
-        :images_cache,
+        # { images: [] },
+        # :images_cache,
+        # bean_images_attributes: %i[id bean_id image image_cache],
       )
   end
 
@@ -74,5 +92,24 @@ class BeansController < ApplicationController
 
   def set_cropped_at
     params[:bean][:cropped_at] = "#{params[:bean][:cropped_at]}-01"
+  end
+
+  def set_new_images
+    return unless params.include?(:bean_images)
+    @new_images = params[:bean_images]['image']
+  end
+
+  def update_bean_images
+    bean_images = @bean.bean_images
+    bean_images.each(&:destroy)
+    @new_images.each do |img|
+      @bean.bean_images.create(image: img, bean_id: @bean.id)
+    end
+  end
+
+  def max_bean_images_count_render_for(view)
+    return unless @new_images && @new_images.length > MAX_BEANIMAGE_COUNT
+    flash.now[:alert] = "登録できるイメージは #{MAX_BEANIMAGE_COUNT}までです"
+    render view
   end
 end
--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,7 +1,8 @@
 class Bean < ApplicationRecord
   belongs_to :roaster
   has_many :bean_images, dependent: :destroy
-  accepts_nested_attributes_for :bean_images, allow_destroy: true
+
+  # accepts_nested_attributes_for :bean_images, allow_destroy: true
   default_scope -> { order(created_at: :desc) }
   validates :roaster_id, presence: true
   validates :name, presence: true
@@ -19,5 +20,4 @@ class Bean < ApplicationRecord
               maximum: 4,
               too_long: '画像は４枚まで投稿できます',
             }
-  # mount_uploaders :images, ImageUploader
 end
