2fc2ffe5421cc5cd01114ea2c2addc7997deff35
tatadkn <tatadaikn@gmail.com>
2021-11-27 18:47:45 +0900

    feat(mst_roast_levels.rb): seed-fuによるmst_roast_levelsデータの作成

---
+++ db/fixtures/00_mst_roaste_levels.rb
@@ -0,0 +1,8 @@
+MstRoasteLevel.seed(
+  :id,
+  { id: 1, name: '浅煎り' },
+  { id: 2, name: '中浅煎り' },
+  { id: 3, name: '中煎り' },
+  { id: 4, name: '中深煎り' },
+  { id: 5, name: '深煎り' },
+)
---
+++ db/fixtures/development/_sample_beans.rb
@@ -0,0 +1,22 @@
+30.times do |n|
+  Bean.seed_once do |s|
+    s.id = n + 1
+    s.name = "テストビーンズ#{n + 1}"
+    s.roaster_id = 1
+    s.created_at = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+    s.country = Faker::Coffee.country
+    s.subregion = Faker::Movies::Hobbit.location
+    s.farm = Faker::BossaNova.song
+    s.variety = Faker::Coffee.variety
+    s.elevation = Faker::Number.within(range: 800..1800)
+    s.process = Faker::Coffee.process
+    s.describe = Faker::Lorem.sentence(word_count: 5)
+    s.cropped_at = Faker::Date.in_date_period(year: 2020)
+    s.acidity = Faker::Number.within(range: 1..5)
+    s.flavor = Faker::Number.within(range: 1..5)
+    s.body = Faker::Number.within(range: 1..5)
+    s.bitterness = Faker::Number.within(range: 1..5)
+    s.sweetness = Faker::Number.within(range: 1..5)
+    s.roaste_level_id = Faker::Number.within(range: 1..5)
+  end
+end
