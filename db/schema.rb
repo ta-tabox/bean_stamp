8d11f39890472eedce34557c66e59620ccd32b30
tatadkn <tatadaikn@gmail.com>
2021-11-27 20:52:27 +0900

    fix(mst_roast_level): 単語ミスの修正 roaste_level -> roast_level

--- db/schema.rb
+++ db/schema.rb
@@ -44,19 +44,27 @@ ActiveRecord::Schema.define(version: 2021_11_28_024423) do
     t.index ["roast_level_id"], name: "index_beans_on_roast_level_id"
 =======
     t.json "images"
-    t.bigint "roaste_level_id", null: false
+    t.bigint "roast_level_id", null: false
     t.index ["country"], name: "index_beans_on_country"
+<<<<<<< HEAD
     t.index ["roaste_level_id"], name: "index_beans_on_roaste_level_id"
 >>>>>>> cc26598 (feat(mst_roaste_level): seed-fuの導入とmst_roaste_levelテーブルの作成、beanモデルとの関連付け)
+=======
+    t.index ["roast_level_id"], name: "index_beans_on_roast_level_id"
+>>>>>>> 7fc6421 (fix(mst_roast_level): 単語ミスの修正 roaste_level -> roast_level)
     t.index ["roaster_id", "created_at"], name: "index_beans_on_roaster_id_and_created_at"
     t.index ["roaster_id"], name: "index_beans_on_roaster_id"
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   create_table "mst_roast_levels", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
 =======
   create_table "mst_roaste_levels", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
 >>>>>>> cc26598 (feat(mst_roaste_level): seed-fuの導入とmst_roaste_levelテーブルの作成、beanモデルとの関連付け)
+=======
+  create_table "mst_roast_levels", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+>>>>>>> 7fc6421 (fix(mst_roast_level): 単語ミスの修正 roaste_level -> roast_level)
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -94,7 +102,10 @@ ActiveRecord::Schema.define(version: 2021_11_28_024423) do
     t.index ["roaster_id"], name: "index_users_on_roaster_id"
   end
 
+<<<<<<< HEAD
   add_foreign_key "bean_images", "beans"
+=======
+>>>>>>> 7fc6421 (fix(mst_roast_level): 単語ミスの修正 roaste_level -> roast_level)
   add_foreign_key "beans", "mst_roast_levels", column: "roast_level_id"
   add_foreign_key "beans", "roasters"
   add_foreign_key "users", "roasters"
