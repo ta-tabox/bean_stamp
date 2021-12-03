d9763c3c77daa22f4e12bad1a0d3bd5f7e2def10
tatadkn <tatadaikn@gmail.com>
2021-12-03 22:22:54 +0900

    fix(seeds.rb): 環境ごとにseedsファイルを切り替える設定をコメントアウトに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # 全ての環境で/seeds/base.rbを読み込む
 load(Rails.root.join('db/seeds/base.rb'))
 # Rails の環境に合わせて読み込むseedsファイルを切り替える
-load(Rails.root.join('db/seeds', "#{Rails.env.downcase}.rb"))
+# load(Rails.root.join('db/seeds', "#{Rails.env.downcase}.rb"))
