d3566bd8a3615dee519860fca5d628a893fee140
tatadkn <tatadaikn@gmail.com>
2021-12-15 21:14:52 +0900

    chore(application.rb): generatorsの設定を追加　rspecファイルの生成のセッティング

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module BeansApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec, fixtures: true, view_specs: false, helper_specs: false, routing_specs: false
+    end
   end
 end
