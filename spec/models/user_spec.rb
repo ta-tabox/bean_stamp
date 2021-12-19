57ed1a8c75bd48fd734bdcd022d3f4ceadb2a58f
tatadkn <tatadaikn@gmail.com>
2021-12-19 21:27:30 +0900

    test(spec/models/user_spec.rb): user modelのバリデーションのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model, focus: true do
+  # name, email, passwordがあれば有効な状態であること
+  it 'is valid with a name, email and password' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  describe 'バリデーションのテスト' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  end
 end
