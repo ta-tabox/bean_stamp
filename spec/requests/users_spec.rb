209da0309c2c47affcfb1f7b2189cb2a76c2f6f0
tatadkn <tatadaikn@gmail.com>
2021-12-25 10:44:03 +0900

    test(requests/users_spec.rb): usersリクエストスペックのリファクタリング

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,24 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
   describe 'GET /home' do
-    before do
-      @user = create(:user)
-    end
     it 'returns http success' do
-      sign_in @user
       get '/users/home'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
-    before do
-      @user = create(:user)
-    end
     it 'returns http success' do
-      sign_in @user
-      get "/users/#{@user.id}"
+      get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
     end
   end
