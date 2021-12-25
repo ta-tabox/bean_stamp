e4cf9e059baed6b08c62ff33f3520bcd128022f6
tatadkn <tatadaikn@gmail.com>
2021-12-25 15:39:32 +0900

    test(requests/static_pages_spec.rb): static_pagesリクエストスペック タイトルのテストの追加と#homeリダイレクトのテストの追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'StaticPages', type: :request do
-  describe 'GET /home' do
-    it 'returns http success' do
-      get '/home'
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:base_title) { 'BeansApp' }
+
+  describe 'GET #home' do
+    context 'when with sign out' do
+      it 'gets home' do
+        get home_url
+        expect(response).to have_http_status(:success)
+        expect(response.body).to match(%r{<title>TOP | #{base_title}</title>}i)
+      end
+    end
+
+    context 'when with sign in' do
+      it 'redirects to user_home' do
+        sign_in user
+        get home_url
+        expect(response).to redirect_to(user_home_path)
+      end
     end
   end
 
-  describe 'GET /help' do
-    it 'returns http success' do
-      get '/help'
+  describe 'GET #help' do
+    it 'gets help' do
+      get help_url
       expect(response).to have_http_status(:success)
+      expect(response.body).to match(%r{<title>HELP | #{base_title}</title>}i)
     end
   end
 
-  describe 'GET /about' do
-    it 'returns http success' do
-      get '/about'
+  describe 'GET #about' do
+    it 'gets about' do
+      get about_url
       expect(response).to have_http_status(:success)
+      expect(response.body).to match(%r{<title>ABOUT | #{base_title}</title>}i)
     end
   end
 end
