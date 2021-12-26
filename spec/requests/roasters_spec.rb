b491625472b3baccd46109e20495197614531116
tatadkn <tatadaikn@gmail.com>
2021-12-26 23:16:15 +0900

    test(requests/roaster_spec): ロースター未登録時のcreateアクションのテストに対し失敗時のテストを追記

--- spec/requests/roasters_spec.rb
+++ spec/requests/roasters_spec.rb
@@ -6,6 +6,94 @@ RSpec.describe 'Roasters', type: :request do
   let(:user_belonging_a_roaster) { create(:user, roaster: roaster) }
   let(:user_not_belonging_a_roaster) { create(:user) }
 
+  # ロースター未登録の場合のテスト
+  context 'when user is not belonging to the roaster' do
+    before do
+      sign_in user_not_belonging_a_roaster
+    end
+
+    describe 'GET #index' do
+      it 'gets roasters/index' do
+        get roasters_path
+        expect(response).to have_http_status(:success)
+        skip 'roaster/indexのタイトル' do
+          # roaster/indexのタイトルを決めたらテストする（search機能実装時）
+          expect(response.body).to include("<title>#{base_title}</title>")
+        end
+      end
+    end
+
+    describe 'GET #show' do
+      it 'gets roasters/show' do
+        get roaster_path roaster
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include("<title>ロースター詳細#{base_title}</title>")
+      end
+    end
+
+    describe 'GET #new' do
+      it 'gets roasters/new' do
+        get new_roaster_path
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include("<title>ロースター登録#{base_title}</title>")
+      end
+    end
+
+    describe 'POST #create' do
+      context 'with valid parameter' do
+        it 'create a Roaster and redirect_to root_path' do
+          expect do
+            post roasters_path, params: { roaster: attributes_for(:roaster) }
+          end.to change(Roaster, :count).by(1)
+          expect(response).to redirect_to roaster_path(Roaster.last)
+        end
+      end
+
+      context 'with invalid parameter' do
+        it 'does not create a Raster and redirects to root_path' do
+          expect do
+            post roasters_path, params: { roaster: attributes_for(:roaster, :invalid) }
+          end.to_not change(Roaster, :count)
+          expect(response).to have_http_status(:success)
+          expect(response.body).to include("<title>ロースター登録#{base_title}</title>")
+        end
+
+        it 'shows a error message' do
+          post roasters_path, params: { roaster: attributes_for(:roaster, :invalid) }
+          expect(response.body).to include '1 件のエラーが発生したため ロースター は保存されませんでした'
+        end
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'redirects to root_path ' do
+        get edit_roaster_path roaster
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'PUT #update' do
+      it 'redirects to root_path ' do
+        put roaster_path roaster, params: { roaster: attributes_for(:roaster, :update) }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'DELETE #destory' do
+      it 'redirects to root_path ' do
+        expect { delete roaster_path roaster }.not_to change(Roaster, :count)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'GET #cancel' do
+      it 'redirects to root_path ' do
+        get cancel_roasters_path
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
   # ロースター登録済みの場合のテスト
   context 'when user is belonging to the roaster' do
     before do
@@ -84,77 +172,6 @@ RSpec.describe 'Roasters', type: :request do
     end
   end
 
-  # ロースター未登録の場合のテスト
-  context 'when user is not belonging to the roaster' do
-    before do
-      sign_in user_not_belonging_a_roaster
-    end
-
-    describe 'GET #index' do
-      it 'gets roasters/index' do
-        get roasters_path
-        expect(response).to have_http_status(:success)
-        skip 'roaster/indexのタイトル' do
-          # roaster/indexのタイトルを決めたらテストする（search機能実装時）
-          expect(response.body).to include("<title>#{base_title}</title>")
-        end
-      end
-    end
-
-    describe 'GET #show' do
-      it 'gets roasters/show' do
-        get roaster_path roaster
-        expect(response).to have_http_status(:success)
-        expect(response.body).to include("<title>ロースター詳細#{base_title}</title>")
-      end
-    end
-
-    describe 'GET #new' do
-      it 'gets roasters/new' do
-        get new_roaster_path
-        expect(response).to have_http_status(:success)
-        expect(response.body).to include("<title>ロースター登録#{base_title}</title>")
-      end
-    end
-
-    describe 'POST #create' do
-      it 'create a Roaster and redirect_to root_path' do
-        expect do
-          post roasters_path, params: { roaster: attributes_for(:roaster) }
-        end.to change(Roaster, :count).by(1)
-        expect(response).to redirect_to roaster_path(Roaster.last)
-      end
-    end
-
-    describe 'GET #edit' do
-      it 'redirects to root_path ' do
-        get edit_roaster_path roaster
-        expect(response).to redirect_to root_path
-      end
-    end
-
-    describe 'PUT #update' do
-      it 'redirects to root_path ' do
-        put roaster_path roaster, params: { roaster: attributes_for(:roaster, :update) }
-        expect(response).to redirect_to root_path
-      end
-    end
-
-    describe 'DELETE #destory' do
-      it 'redirects to root_path ' do
-        expect { delete roaster_path roaster }.not_to change(Roaster, :count)
-        expect(response).to redirect_to root_path
-      end
-    end
-
-    describe 'GET #cancel' do
-      it 'redirects to root_path ' do
-        get cancel_roasters_path
-        expect(response).to redirect_to root_path
-      end
-    end
-  end
-
   # ユーザーが別のロースターに所属していた場合のテスト
   context 'when user is belonging to another roaster' do
     let(:another_roaster) { create(:roaster, name: '別のロースター') }
@@ -203,6 +220,7 @@ RSpec.describe 'Roasters', type: :request do
     end
   end
 
+  # ゲストロースター編集、削除のテスト ensure_normal_roaster
   context 'when sign in as a guest user' do
     let!(:guest_user) { create(:user, :guest, roaster: guest_roaster) }
     let!(:guest_roaster) { create(:roaster, :guest) }
@@ -211,7 +229,6 @@ RSpec.describe 'Roasters', type: :request do
       sign_in guest_user
     end
 
-    # ゲストロー���ター編集、削除のテスト ensure_normal_roaster
     describe 'POST #update' do
       it 'does not updated the guest roaster and redirects to root_path' do
         expect do
