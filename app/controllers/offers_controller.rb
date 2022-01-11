3291d8a92fec8e7312d13b68cfc47c6f9b4bd9bd
tatadkn <tatadaikn@gmail.com>
2022-01-11 22:45:23 +0900

    feat(offers/show.html.erb): offerリソースの#showの追加

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,7 @@
 class OffersController < ApplicationController
   before_action :user_signed_in_required
   before_action :user_belonged_to_roaster_required
-  # オファーの期日関連のバリデーションを追加する
-  # オファー開始日は当日以降でないといけない且つ＜1ヶ月後
-  # 焙煎日はオファー開始日＜焙煎日＜＝受け取り開始日且つ＜1ヶ月後
-  # 受け取り開始日は焙煎日＜受け取り開始日＜受け取り終了日且つ＜1ヶ月後
-  # 受け取り終了日は受け取り開始日＜受け取り終了日＜2ヶ月後
+  before_action :set_offer, only: %i[show edit update destroy]
 
   def index
     @pagy, @offers = pagy(current_roaster.offers)
@@ -47,5 +43,12 @@ class OffersController < ApplicationController
     return if @bean
 
     redirect_to beans_path, alert: 'コーヒー豆を登録してください'
+  end
+
+  def set_offer
+    @offer = current_roaster.offers.find_by(id: params[:id])
+    return if @offer
+
+    redirect_to beans_path, alert: 'オファーを登録してください'
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,6 +9,11 @@ class Offer < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true }
   validates :weight, presence: true, numericality: { only_integer: true }
   validates :amount, presence: true, numericality: { only_integer: true }
+  # オファーの期日関連のバリデーションを追加する
+  # オファー開始日は当日以降でないといけない且つ＜1ヶ月後
+  # 焙煎日はオファー開始日＜焙煎日＜＝受け取り開始日且つ＜1ヶ月後
+  # 受け取り開始日は焙煎日＜受け取り開始日＜受け取り終了日且つ＜1ヶ月後
+  # 受け取り終了日は受け取り開始日＜受け取り終了日＜2ヶ月後
 
   # オファーが所属するロースターを返す
   def roaster
