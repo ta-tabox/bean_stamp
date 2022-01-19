c53ee05bfcb8c5be052ae95e65bd7455a8e598a5
tatadkn <tatadaikn@gmail.com>
2022-01-19 23:12:19 +0900

    fix(users_controller.rb): #homeについてOfferデータ呼び出し時にinclude([:bean])を追加

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,7 +2,7 @@ class Users::UsersController < ApplicationController
   before_action :authenticate_user!
   def home
     # フォローリソースを実装するときにユーザーがフォローするロースターのオファーのみに修正する
-    @pagy, @offers = pagy(Offer.all)
+    @pagy, @offers = pagy(Offer.all.includes([:bean]))
   end
 
   def show
