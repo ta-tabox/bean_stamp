2023a696eda3ad557c30648afbda8ebac7cb1145
tatadkn <tatadaikn@gmail.com>
2022-01-23 16:21:03 +0900

    fix(offer一覧): users#home, offers#indexのoffer/_formを表示するクエリについて、includes(bean: :bean_images)を追加

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,7 +2,7 @@ class Users::UsersController < ApplicationController
   before_action :authenticate_user!
   def home
     # フォローリソースを実装するときにユーザーがフォローするロースターのオファーのみに修正する
-    @pagy, @offers = pagy(Offer.all.includes(%i[bean roaster]))
+    @pagy, @offers = pagy(Offer.all.includes(:roaster, bean: :bean_images))
   end
 
   def show
