5b332f2a61c4dfcdd06c9def09a8d1a200775c8b
tatadkn <tatadaikn@gmail.com>
2022-01-23 22:47:25 +0900

    feat(roasters/show.html.erb): フォロー、アンフォローボタンのAjax化

--- app/controllers/roaster_relationships_controller.rb
+++ app/controllers/roaster_relationships_controller.rb
@@ -1,14 +1,20 @@
 class RoasterRelationshipsController < ApplicationController
   before_action :user_signed_in_required
   def create
-    roaster = Roaster.find(params[:roaster_id])
-    current_user.follow_roaster(roaster)
-    redirect_to roaster
+    @roaster = Roaster.find(params[:roaster_id])
+    current_user.follow_roaster(@roaster)
+    respond_to do |format|
+      format.html { redirect_to @roaster }
+      format.js
+    end
   end
 
   def destroy
-    roaster = RoasterRelationship.find(params[:id]).roaster
-    current_user.unfollow_roaster(roaster)
-    redirect_to roaster
+    @roaster = RoasterRelationship.find(params[:id]).roaster
+    current_user.unfollow_roaster(@roaster)
+    respond_to do |format|
+      format.html { redirect_to @roaster }
+      format.js
+    end
   end
 end
