b5a3781e0327beab1800c57eba31715d003fb37a
tatadkn <tatadaikn@gmail.com>
2022-01-29 16:48:14 +0900

    feat(users#wants): users#wantsアクションの作成、users/wantsページの作成

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,15 +1,24 @@
 class Users::UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show following]
+
   def home
     @pagy, @offers = pagy(Offer.following_by(current_user))
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def following
-    @user = User.find(params[:id])
     @pagy, @roasters = pagy(@user.following_roasters)
+  end
+
+  def wants
+    @pagy, @offers = pagy(current_user.wanting_offers.includes(:roaster, bean: :bean_images))
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
