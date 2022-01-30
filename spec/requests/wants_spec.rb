305d46fade4d516cfab40b08a42ad586bf725c69
tatadkn <tatadaikn@gmail.com>
2022-01-30 18:26:49 +0900

    test(requests/wants_spec.rb): wantsリソースの#show, #receiptについてのリクエストテストを追加

--- spec/requests/wants_spec.rb
+++ spec/requests/wants_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Wants', type: :request do
   let(:base_title) { ' | BeansApp' }
   let(:user) { create(:user) }
+  let(:another_user) { create(:user, name: '他のユーザー') }
   let!(:roaster) { create(:roaster) }
   let!(:bean) { create(:bean, :with_image, :with_3_taste_tags, roaster: roaster) }
   let!(:offer) { create(:offer, bean: bean) }
@@ -38,6 +39,34 @@ RSpec.describe 'Wants', type: :request do
         subject
         expect(response).to have_http_status(:success)
         expect(response.body).to include("<title>ウォンツしたユーザー#{base_title}</title>")
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    let(:want) { create(:want, user_id: user.id, offer_id: offer.id) }
+    subject { get want_path want }
+    context 'when a user is not signed in' do
+      it 'redirects to new_user_session_path' do
+        subject
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+    context 'when a user does not have a want' do
+      before { sign_in another_user }
+      it 'redirects to wants_users_path' do
+        subject
+        expect(response).to redirect_to wants_users_path
+      end
+    end
+    context 'when a user has a want' do
+      before { sign_in user }
+      it "gets wants/show and shows a want's info" do
+        subject
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include("<title>ウォンツ詳細#{base_title}</title>")
+        expect(response.body).to include want.bean.name
+        expect(response.body).to include want.roaster.name
       end
     end
   end
@@ -86,6 +115,30 @@ RSpec.describe 'Wants', type: :request do
         subject { proc { delete want_path(want), xhr: true } }
         it { is_expected.to change(Want, :count).by(-1) }
       end
+    end
+  end
+
+  describe 'PATCH #receipt' do
+    let(:want) { create(:want, user_id: user.id, offer_id: offer.id) }
+    subject { proc { patch receipt_want_path(want) } }
+    context 'when user is not signed in' do
+      it 'redirects to new_user_session_path ' do
+        subject.call
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when a user does not have a want' do
+      before { sign_in another_user }
+      it 'redirects to wants_users_path' do
+        subject.call
+        expect(response).to redirect_to wants_users_path
+      end
+    end
+
+    context 'when a user have a want' do
+      before { sign_in user }
+      it { is_expected.to change { Want.find(want.id).receipted_at? }.from(false).to(true) }
     end
   end
 end
