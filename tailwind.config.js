2830115b7a7a393bc9ba7ae7c8c66a4fcfaff700
tatadkn <tatadaikn@gmail.com>
2022-02-24 21:25:40 +0900

    feat(button): ボタンのフォームを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+      textColor: ["active"],
+      opacity: ["disabled"],
+    },
   },
   plugins: [],
 };
