8ab897cfcc6fccfd886e1ef900aa4d9024fe7960
tatadkn <tatadaikn@gmail.com>
2022-03-12 15:58:54 +0900

    feat(swiper): bean_imagesのカルーセルの実装

--- app/javascript/shared/swiper.js
+++ app/javascript/shared/swiper.js
@@ -1,20 +1,20 @@
 import Swiper, { Navigation, Pagination } from "swiper";
-import "swiper/swiper-bundle.min.js";
+// import "swiper/swiper-bundle.min.js";
 import "swiper/swiper-bundle.min.css";
 
-// document.addEventListener("turbolinks:load", function () {
-document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".mySwiper", {
+// Tubolinksと併用する場合のイベントバンドラ
+document.addEventListener("turbolinks:load", function () {
+  // document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
     // ナビゲーションとページネーション機能の読み込み
     modules: [Navigation, Pagination],
-    // Optional parameters
-
-    loop: true,
+    // loop: true,
 
     // If we need pagination
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
+      clickable: true,
     },
 
     // Navigation arrows
