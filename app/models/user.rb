6cdfc86759eb481d0644af33143e9aa1136976b4
tatadkn <tatadaikn@gmail.com>
2022-03-16 21:10:15 +0900

    fix(flash_massage): deviseの:timeoutアラートを非表示にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :trackable and :omniauthable
   include JpPrefecture
   devise :database_authenticatable,
          :registerable,
