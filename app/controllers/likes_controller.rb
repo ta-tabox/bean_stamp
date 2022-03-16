ec57188ce63c0ba68f562587a46b8422e672e25c
tatadkn <tatadaikn@gmail.com>
2022-03-16 22:54:14 +0900

    feat(likes_controller.rb): likes関係のルーティングの設定、likes_controller.rbの作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,27 @@
+class LikesController < ApplicationController
+  before_action :user_signed_in_required
+
+  def index
+    offers = current_user.like_offers
+    offers&.map { |offer| offer.update_status }
+    @pagy, @offers = pagy(offers.includes(:roaster, { bean: :roast_level }).recent)
+  end
+
+  def create
+    @offer = Offer.find(params[:offer_id])
+    current_user.like_offers << @offer
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      # format.js
+    end
+  end
+
+  def destroy
+    @offer = Want.find(params[:id]).offer
+    current_user.like_offers.delete(@offer)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      # format.js
+    end
+  end
+end
