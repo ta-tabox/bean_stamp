0a320a7db687a7e8293239f34f52aac0fcb2e059
tatadkn <tatadaikn@gmail.com>
2022-03-19 16:53:21 +0900

    feat(searches_controller.rb): ロースター検索機能をsearches#indexでも実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,19 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @roasters_q = Roaster.ransack(params[:roasters_q], search_key: :roasters_q)
+    @pagy, @roasters = pagy(@roasters_q.result(distinct: true))
+  end
 
   def roaster
-    @q = Roaster.ransack(params[:q])
-    flash.now[:alert] = 'ロースターが見つかりませんでした' unless @q.result.any?
-    @pagy, @roasters = pagy(@q.result(distinct: true))
+    @roasters_q = Roaster.ransack(params[:roasters_q], search_key: :roasters_q)
+    flash.now[:alert] = 'ロースターが見つかりませんでした' unless @roasters_q.result.any?
+    @pagy, @roasters = pagy(@roasters_q.result(distinct: true))
   end
 
-  def offer
-    @q = Offer.ransack(params[:q])
-    @offers = @q.result(distinct: true)
-  end
+  # def offer
+  #   @q = Offer.ransack(params[:q])
+  #   @offers = @q.result(distinct: true)
+  # end
 end
