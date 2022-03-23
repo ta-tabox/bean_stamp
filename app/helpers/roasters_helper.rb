6102af35ad6a426f972c6a92e648d552c543d8c8
tatadkn <tatadaikn@gmail.com>
2022-03-23 21:38:38 +0900

    feat(users_helper.rb): ユーザー用の通知の機能の作成
    ロースト中のウォンツと未受け取りのウォンツの通知を表示する

--- app/helpers/roasters_helper.rb
+++ app/helpers/roasters_helper.rb
@@ -5,37 +5,28 @@ module RoastersHelper
     link_to '編集', edit_roaster_path, class: 'link'
   end
 
-  def note_for_no_actions(offers)
+  # shared/_roaster_asideで使用するNotification
+  def no_notes_for_roaster(offers)
     return if offers.on_roasting.any? || offers.on_selling.any?
 
     tag.p 'お知らせはありません', class: 'text-sm text-gray-500'
   end
 
-  def note_for_on_roasting(offers)
+  # ロースト中のオファーのお知らせ
+  def note_for_on_roasting_offers(offers)
     return unless offers.on_roasting.any?
 
-    if cookies[:roaster_id]
-      link_to search_offers_path(search: Offer.statuses[:on_roasting]) do
-        tag.p "#{Offer.statuses_i18n[:on_roasting]}のオファーが#{offers.on_roasting.count}件あります", class: 'link text-sm'
-      end
-    else
-      link_to search_wants_path(search: Offer.statuses[:on_roasting]) do
-        tag.p "#{Offer.statuses_i18n[:on_roasting]}のウォンツが#{offers.on_roasting.count}件あります", class: 'link text-sm'
-      end
+    link_to search_offers_path(search: Offer.statuses[:on_roasting]) do
+      tag.p "#{Offer.statuses_i18n[:on_roasting]}のオファーが#{offers.on_roasting.count}件あります", class: 'link text-sm'
     end
   end
 
-  def note_for_on_selling(offers)
+  # 販売中のオファーのお知らせ
+  def note_for_on_selling_offers(offers)
     return unless offers.on_selling.any?
 
-    if cookies[:roaster_id]
-      link_to search_offers_path(search: Offer.statuses[:on_selling]) do
-        tag.p "#{Offer.statuses_i18n[:on_selling]}のオファーが#{offers.on_selling.count}件あります", class: 'link text-sm'
-      end
-    else
-      link_to search_wants_path(search: Offer.statuses[:on_selling]) do
-        tag.p "#{Offer.statuses_i18n[:on_selling]}のウォンツが#{offers.on_selling.count}件あります", class: 'link text-sm'
-      end
+    link_to search_offers_path(search: Offer.statuses[:on_selling]) do
+      tag.p "#{Offer.statuses_i18n[:on_selling]}のオファーが#{offers.on_selling.count}件あります", class: 'link text-sm'
     end
   end
 end
--- app/helpers/users/users_helper.rb
+++ app/helpers/users/users_helper.rb
@@ -4,4 +4,35 @@ module Users::UsersHelper
 
     link_to '編集', edit_user_registration_path, class: 'link'
   end
+
+  # shared/_user_asideで使用するNotification
+  # ロースト中のウォンツと未受け取りのウォンツのお知らせがない時
+  def no_notes_for_user(wants)
+    return if note_for_on_roasting_wants(wants) || note_for_unreceipted_wants(wants)
+
+    tag.p 'お知らせはありません', class: 'text-sm text-gray-500'
+  end
+
+  # ロースト中のウォンツのお知らせ
+  def note_for_on_roasting_wants(wants)
+    on_roasting_wants = wants.includes(:offer).select { |want| want.offer.on_roasting? == true }
+    return unless on_roasting_wants.any?
+
+    link_to search_wants_path(search: Offer.statuses[:on_roasting]) do
+      tag.p "#{Offer.statuses_i18n[:on_roasting]}のウォンツが#{on_roasting_wants.count}件あります", class: 'link text-sm'
+    end
+  end
+
+  # 未受け取りのウォンツのお知らせ
+  def note_for_unreceipted_wants(wants)
+    on_selling_wants = wants.includes(:offer).select { |want| want.offer.on_selling? == true }
+    return unless on_selling_wants.any?
+
+    unreceipted_wants = on_selling_wants.select { |want| want.receipted_at? == false }
+    return unless unreceipted_wants.any?
+
+    link_to search_wants_path(search: Offer.statuses[:on_selling]) do
+      tag.p "未受け取りのウォンツが#{unreceipted_wants.count}件あります", class: 'link text-sm'
+    end
+  end
 end
