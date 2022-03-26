42f4888c4c647ca4468c5370f097bad2b0148f66
tatadkn <tatadaikn@gmail.com>
2022-03-26 19:16:31 +0900

    test(system/users_spec.rb): user#homeのオファーコンテンツのテストを追加
    Overview, Taste, Scheduleタグの機能のテスト

--- spec/system/roaster_relationships_spec.rb
+++ spec/system/roaster_relationships_spec.rb
@@ -9,26 +9,6 @@ RSpec.describe 'RoasterRelationships', type: :system do
 
   before { sign_in user }
 
-  describe 'User#home' do
-    let!(:bean) { create(:bean, :with_image, :with_3_taste_tags, name: 'following_bean', roaster: following_roaster) }
-    let!(:offer) { create(:offer, bean: bean) }
-    let!(:another_bean) { create(:bean, :with_image, :with_3_taste_tags, name: 'another_bean', roaster: another_roaster) }
-    let!(:another_offer) { create(:offer, bean: another_bean) }
-
-    before do
-      user.following_roasters << following_roaster
-      visit home_users_path
-    end
-
-    # フォローユーザーのオファーのみを表示させる
-    it 'shows an offer which is had by the roaster a user follow' do
-      expect(page).to have_content bean.name
-      expect(page).to have_content following_roaster.name
-      expect(page).to_not have_content another_bean.name
-      expect(page).to_not have_content another_roaster.name
-    end
-  end
-
   describe 'User#following' do
     before do
       user.following_roasters << following_roaster
