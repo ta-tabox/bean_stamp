e315f38cf1a059d0efbe3907f50c35c2b324e817
tanktabox <tanktabox@gmail.com>
2022-06-11 12:49:33 +0900

    feat(asset_sync.rb): asset_syncの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   # config.assets.initialize_on_precompile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # AWS CloudFrontを設定
   config.action_controller.asset_host = Rails.application.credentials.dig(:aws, :s3_host)
   config.asset_host = Rails.application.credentials.dig(:aws, :s3_host)
 
