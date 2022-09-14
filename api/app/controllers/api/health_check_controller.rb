a8062a740c33c4966ed3db3183bc38e1efc96492
tanktabox <tanktabox@gmail.com>
2022-09-14 21:39:13 +0900

    fix: mst_countries.rb修正に伴うdemoデータのfixturesの修正

--- api/app/controllers/api/health_check_controller.rb
+++ api/app/controllers/api/health_check_controller.rb
@@ -1,5 +1,7 @@
 class Api::HealthCheckController < Api::ApplicationController
   def index
     head :ok
+    # NOTE: execTest()によるapiとの疎通確認用 不要になったら消す
+    # render json: { status: :ok, message: 'ok' }
   end
 end
