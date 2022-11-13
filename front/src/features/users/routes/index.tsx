e14ab45b84fd1743d537fc725bf1adfb67cd63fa
tanktabox <tanktabox@gmail.com>
2022-11-13 23:26:27 +0900

    feat(front:): パスワード変更機能の実装

--- front/src/features/users/routes/index.tsx
+++ front/src/features/users/routes/index.tsx
@@ -7,6 +7,7 @@ import { UserCancel } from '@/features/users/components/pages/UserCancel'
 import { UserEdit } from '@/features/users/components/pages/UserEdit'
 import { UserHome } from '@/features/users/components/pages/UserHome'
 import { UserManagement } from '@/features/users/components/pages/UserManagement'
+import { UserPassword } from '@/features/users/components/pages/UserPassword'
 
 export const UsersRoutes: FC = () => (
   <Routes>
@@ -14,6 +15,7 @@ export const UsersRoutes: FC = () => (
       <Route index element={<UserHome />} />
       <Route path="home" element={<UserHome />} />
       <Route path="edit" element={<UserEdit />} />
+      <Route path="password" element={<UserPassword />} />
       <Route path="management" element={<UserManagement />} />
       <Route path="cancel" element={<UserCancel />} />
       <Route path=":id" element={<User />} />
