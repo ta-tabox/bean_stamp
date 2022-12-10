1700663ce68d86a24472119eeb083a8daa36110f
tanktabox <tanktabox@gmail.com>
2022-12-10 20:33:33 +0900

    fix(users routes): isRoaster ture時にusers/へのアクセスを制限する

--- front/src/features/users/routes/index.tsx
+++ front/src/features/users/routes/index.tsx
@@ -8,16 +8,18 @@ import { UserEdit } from '@/features/users/components/pages/UserEdit'
 import { UserFollowing } from '@/features/users/components/pages/UserFollowing'
 import { UserHome } from '@/features/users/components/pages/UserHome'
 import { UserPassword } from '@/features/users/components/pages/UserPassword'
+import { RequireNotIsRoaster } from '@/router/RequireNotIsRoaster'
 
 export const UsersRoutes: FC = () => (
-  // TODO isRoaster trueの時にルーティングの制限を行う。:id, :id/following以外はroasters/homeにリダイレクト
   <Routes>
     <Route element={<MainLayout />}>
-      <Route index element={<UserHome />} />
-      <Route path="home" element={<UserHome />} />
-      <Route path="edit" element={<UserEdit />} />
-      <Route path="password" element={<UserPassword />} />
-      <Route path="cancel" element={<UserCancel />} />
+      <Route element={<RequireNotIsRoaster />}>
+        <Route index element={<UserHome />} />
+        <Route path="home" element={<UserHome />} />
+        <Route path="edit" element={<UserEdit />} />
+        <Route path="password" element={<UserPassword />} />
+        <Route path="cancel" element={<UserCancel />} />
+      </Route>
       <Route path=":id" element={<User />} />
       <Route path=":id/following" element={<UserFollowing />} />
       <Route path="*" element={<Navigate to="." />} />
