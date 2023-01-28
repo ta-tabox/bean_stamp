2dc91c42abfa0b2019725623fd96a10479e78fb0
tanktabox <tanktabox@gmail.com>
2023-01-28 16:40:55 +0900

    feat(front: user home): ユーザーホームにオファー更新機能を実装

--- front/src/components/Elements/Spinner/Spinner.tsx
+++ front/src/components/Elements/Spinner/Spinner.tsx
@@ -1,5 +1,10 @@
 import { memo } from 'react'
 
 export const Spinner = memo(() => (
-  <div className="animate-spin h-6 w-6 border-4 border-gray-400 rounded-full border-t-transparent" />
+  // <div className="animate-spin h-6 w-6 border-2 border-gray-400 rounded-full border-t-transparent" />
+  <div className="animate-spin">
+    <svg className="h-6 w-6 text-gray-500">
+      <use xlinkHref="#arrow-path" />
+    </svg>
+  </div>
 ))
--- front/src/features/users/components/pages/UserHome.tsx
+++ front/src/features/users/components/pages/UserHome.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import { useEffect, memo } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
+import { SecondaryButton } from '@/components/Elements/Button'
 import { ContentHeader, ContentHeaderTitle } from '@/components/Elements/Content'
 import { Link } from '@/components/Elements/Link'
 import { Pagination } from '@/components/Elements/Pagination'
@@ -16,7 +17,7 @@ export const UserHome: FC = memo(() => {
   const { signedInUser } = useSignedInUser()
   const { loadUser } = useLoadUser()
   const { currentOffers, getCurrentOffers, loading, totalPage, currentPage } = useGetCurrentOffers()
-  const [searchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
 
   // NOTE サインイン直後のユーザー＆ロースター情報の更新はここでOKか？
   useEffect(() => {
@@ -28,6 +29,11 @@ export const UserHome: FC = memo(() => {
     getCurrentOffers({ page: searchParams.get('page') })
   }, [searchParams])
 
+  const onClickReload = () => {
+    getCurrentOffers({ page: null })
+    setSearchParams({})
+  }
+
   return (
     <>
       <Head title="ホーム" />
@@ -37,12 +43,18 @@ export const UserHome: FC = memo(() => {
         </div>
       </ContentHeader>
 
-      {/* ローディング */}
-      {loading && (
-        <div className="flex justify-center">
-          <Spinner />
-        </div>
-      )}
+      {/* オファー更新ボタン */}
+      <div className="flex justify-center">
+        <SecondaryButton onClick={onClickReload}>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <svg className="h-6 w-6 text-gray-500">
+              <use xlinkHref="#arrow-path" />
+            </svg>
+          )}
+        </SecondaryButton>
+      </div>
 
       {!loading && (
         <>
@@ -53,7 +65,7 @@ export const UserHome: FC = memo(() => {
                 <>
                   <ol>
                     {currentOffers.map((offer) => (
-                      <li key={offer.id} className="my-20">
+                      <li key={offer.id} className="mb-20">
                         <OfferContentCard offer={offer} />
                       </li>
                     ))}
@@ -62,7 +74,8 @@ export const UserHome: FC = memo(() => {
                 </>
               ) : (
                 <div className="text-center text-gray-400">
-                  <Link to="/beans">ロースターをフォローしオファーを受け取る</Link>
+                  <p>オファーがありません</p>
+                  <Link to="/search">ロースターをフォローしてオファーを受け取る</Link>
                 </div>
               )}
             </section>
