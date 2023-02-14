e01fc7ea8b6049ca176e8f01e958e59d495c3bb0
tanktabox <tanktabox@gmail.com>
2023-02-14 23:16:32 +0900

    fix(wants/utils): 日付による条件分岐関数の修正

--- front/src/features/wants/utils/isAfterEndedAt.ts
+++ front/src/features/wants/utils/isAfterEndedAt.ts
@@ -8,7 +8,9 @@ export const isAfterEndedAt = ({ offer }: Options) => {
   const now = new Date()
   const rowDate = new Date(offer.endedAt)
   const [year, month, date] = [rowDate.getFullYear(), rowDate.getMonth(), rowDate.getDate()]
-  const endedAt = new Date(year, month, date)
+
+  // endedAtは日付データのため、その日の終わりを設定
+  const endedAt = new Date(year, month, date, 23, 59, 59, 99)
 
   return now > endedAt
 }
