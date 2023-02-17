b852630b244dcbcde3eb3962ffe0091e9cdc9493
tanktabox <tanktabox@gmail.com>
2023-02-17 22:18:34 +0900

    feat(reset states): グローバルステートのリセットを修正

--- front/src/features/auth/hooks/useResetStates.ts
+++ front/src/features/auth/hooks/useResetStates.ts
@@ -1,13 +1,17 @@
 import { useSignedInUser } from '@/features/auth/hooks/useSignedInUser'
+import { useLikes } from '@/features/likes'
 import { useRecommendedOffers } from '@/features/offers/hooks/useRecommendedOffers'
-import { useCurrentRoaster } from '@/features/roasters'
-import { useWantsStats } from '@/features/wants'
+import { useCurrentRoaster, useUsersFollowingToRoaster } from '@/features/roasters'
+import { useWants, useWantsStats } from '@/features/wants'
 
 export const useResetStates = () => {
   const { setIsSignedIn, setSignedInUser, setIsBelongingToRoaster } = useSignedInUser()
   const { setCurrentRoaster, setIsRoaster } = useCurrentRoaster()
+  const { setUsersFollowingToRoaster } = useUsersFollowingToRoaster()
   const { setRecommendedOffers, setRecommendedOffersPool } = useRecommendedOffers()
+  const { setWants } = useWants()
   const { setWantsStats } = useWantsStats()
+  const { setLikes } = useLikes()
 
   const resetStates = () => {
     setIsSignedIn(false) // IsSignedInStateを初期化
@@ -15,10 +19,12 @@ export const useResetStates = () => {
     setIsBelongingToRoaster(false) // IsBelongingToRoasterStateを初期化
     setCurrentRoaster(null) // CurrentRoasterStateを初期化
     setIsRoaster(false) // IsRoasterStateを初期化
+    setUsersFollowingToRoaster(null) // usersFollowingToRoasterStateの初期化
     setRecommendedOffers([]) // おすすめのオファーを初期化
     setRecommendedOffersPool([]) // おすすめのオファーのプールを初期化
+    setWants(null) // wantsStateの初期化
     setWantsStats(null) // サインインユーザーのウォント統計を初期化
-    // TODO recoil管理下のステートを追加する
+    setLikes(null) // likesStateの初期化
   }
 
   return { resetStates }
