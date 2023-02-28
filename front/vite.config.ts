752f5318b87b704802634a86dbbc8dd535c6ad80
tanktabox <tanktabox@gmail.com>
2023-02-28 22:56:43 +0900

    feat(front: vite): 本番環境デプロイに対応

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,20 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }], // tsconfigのpathをviteで解決
-  },
-  plugins: [react()],
-  server: {
-    host: true, // コンテナ上で起動する際に必須
-    port: 3000,
-  },
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === 'production'
+  const env = loadEnv(mode, process.cwd(), '')
+  const productionBaseUrl = `${env.VITE_FRONT_ORIGIN}/`
+  return {
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }], // tsconfigのpathをviteで解決
+    },
+    plugins: [react()],
+    server: {
+      host: true, // コンテナ上で起動する際に必須
+      port: 3000,
+    },
+    // CloudFront のディストリビューションドメイン名を設定
+    base: isProduction ? productionBaseUrl : '',
+  }
 })
