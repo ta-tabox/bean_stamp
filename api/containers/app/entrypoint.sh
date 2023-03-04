a90e13290d777b60a6cd86af98c5e7c18126a658
tanktabox <tanktabox@gmail.com>
2023-03-04 22:18:18 +0900

    feat: dockerfileの修正とコンテナデプロイ

--- api/containers/app/entrypoint.sh
+++ api/containers/app/entrypoint.sh
@@ -5,8 +5,9 @@ rm -f /bean_stamp/tmp/pids/server.pid
 
 # コンテナ起動時のdbコマンド
 if [ "$RAILS_ENV" = "production" ]; then
+  # bundle exec rails db:create RAILS_ENV=production # 初回のみ実行
   bundle exec rails db:migrate RAILS_ENV=production
-  # bundle exec rails db:seed RAILS_ENV=production
+  # bundle exec rails db:seed RAILS_ENV=production # 初回のみ実行
 fi
 
 exec "$@"
