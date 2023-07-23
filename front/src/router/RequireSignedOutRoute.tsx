a5680f53e3243c3963913ced528b32ba79b503f3
tanktabox <tanktabox@gmail.com>
2023-07-23 13:08:18 +0900

    fix(front/src/router/): react routerにおいて不要な条件分岐を削除

--- front/src/router/RequireSignedOutRoute.tsx
+++ front/src/router/RequireSignedOutRoute.tsx
@@ -1,23 +1,18 @@
-import type { ReactElement } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 import { useSignedInUser } from '@/features/auth'
 
 type Props = {
   redirectPath?: string
-  children?: ReactElement
 }
 
 export const RequireSignedOutRoute = (props: Props) => {
-  const { redirectPath = '/users/home', children } = props
+  const { redirectPath = '/users/home' } = props
   const { isSignedIn } = useSignedInUser()
 
   if (isSignedIn) {
     return <Navigate to={redirectPath} replace />
   }
 
-  if (children) {
-    return children
-  }
   return <Outlet />
 }
